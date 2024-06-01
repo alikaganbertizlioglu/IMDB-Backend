2532bf4bd6fd28d16d7187cae6102cb41ef82c7e
Ali Kagan Bertizlioglu <alikaganbertizlioglu@gmail.com>
2024-06-02 02:16:11 +0300

    commit

--- src/main/java/com/alikagan/se3355final/config/CorsConfig.java
+++ src/main/java/com/alikagan/se3355final/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Adjust this mapping to match your API endpoints
-                .allowedOrigins("http://localhost:4200","https://wonderful-mushroom-0269ae10f.5.azurestaticapps.net")
+                .allowedOrigins("https://wonderful-mushroom-0269ae10f.5.azurestaticapps.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
